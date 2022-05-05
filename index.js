3ea58dc5189d2ba48f71dd9cb85cecf72a5df16b
Brigham <brig.johnson@gmail.com>
2022-05-05 17:49:41 -0400

    commit tree 84c478f1f2f88e6457b4dc068404450a6b21c531

--- index.js
+++ index.js
@@ -1,3 +1,35 @@
 const batteryBatches = [4, 5, 3, 4, 4, 6, 5];
 
-// Code your solution here
+let totalBatteries = batteryBatches.reduce(
+    ( previousValue, currentValue ) => previousValue + currentValue,
+    0 // where this is the initial amount
+ );
+console.log(totalBatteries);
+
+
+// Other stuff from:
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/Reduce
+const doubledAndSummed = [1, 2, 3].reduce(
+    function(accumulator, element){
+        return element * 2 + accumulator
+    }, 0)
+// => 12
+
+console.log(doubledAndSummed);
+
+let total = [ 0, 1, 2, 3 ].reduce(
+    ( previousValue, currentValue ) => previousValue + currentValue,
+    30 // where this is the initial amount
+  )
+console.log(total);
+
+
+const doubledAndSummed2 = [1, 2, 3].reduce(
+    function(accumulator, element){ return element * 2 + accumulator});
+console.log(doubledAndSummed2)
+// => 11
+
+const doubledAndSummedFromTen = [1, 2, 3].reduce(
+    function(accumulator, element){ return element * 2 + accumulator}, 10);
+// => 22
+console.log(doubledAndSummedFromTen)
